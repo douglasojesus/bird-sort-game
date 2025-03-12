7410a41b3f1db317184b950523fb45a2fba1861d
Douglas <dougecomp@gmail.com>
2025-03-12 12:10:30 +0000

    instructions: how implements and presentation

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from functions import *
 import algorithms
 
+# criar possibilidade de iniciar jogo a partir de um tabuleiro criado pelo professor
+# ele insere os galhos e como eles vão estar ajustados, ao invés de aleatoriamente
+# o jogo gráfico pode ser só para o humano; a parte do uso do algoritmos pode estar para o terminal
+# a heurística pode ser pensada como uma "vantagem" em um jogo real: o problema é codificar isso
+# sobre a apresentação, vai depender da escolha do grupo
+
 def main():
     galhos = int(input('Número de galhos: '))
     tabuleiro = define_tabuleiro(galhos, 0)
@@ -39,7 +45,7 @@ def main():
                 print(f"Mover de {movimento[0]} para {movimento[1]}")
         else:
             print("Não foi possível encontrar uma solução.")
-            
+
     else:
         while True:
             
